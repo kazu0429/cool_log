44761c593386fd243638a3e1d6bd32f794eb3e26
kazu0412 <k.shouhi0412@i.softbank.jp>
2023-09-11 23:59:20 +0900

    add appheader

--- lib/calendar_page.dart
+++ lib/calendar_page.dart
@@ -5,8 +5,33 @@ class CalendarPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("calender")),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 100.0,
+          flexibleSpace: FlexibleSpaceBar(
+              title: const Text(
+                "Calendar",
+                style: TextStyle(
+                  shadows: <Shadow>[
+                    Shadow(
+                      color: Colors.grey,
+                      offset: Offset(5.0, 5.0),
+                      blurRadius: 3.0,
+                    ),
+                  ],
+                ),
+              ),
+              background: FittedBox(
+                fit: BoxFit.cover,
+                child: Image.asset(
+                  "assets/images/undraw_Calendar_re_ki49.png",
+                  width: 10,
+                ),
+              )),
+        ),
+      ],
     );
   }
 }
--- lib/user_page.dart
+++ lib/user_page.dart
@@ -5,8 +5,33 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("user")),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 100.0,
+          flexibleSpace: FlexibleSpaceBar(
+              title: const Text(
+                "Profile",
+                style: TextStyle(
+                  shadows: <Shadow>[
+                    Shadow(
+                      color: Colors.grey,
+                      offset: Offset(5.0, 5.0),
+                      blurRadius: 3.0,
+                    ),
+                  ],
+                ),
+              ),
+              background: FittedBox(
+                fit: BoxFit.cover,
+                child: Image.asset(
+                  "assets/images/undraw_sweet_home_dkhr.png",
+                  width: 10,
+                ),
+              )),
+        ),
+      ],
     );
   }
 }
